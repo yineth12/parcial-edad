7421cfa96d3ebb2941fb993a5f74784d28bfdbc2
Yinel Ayala <>
2021-04-27 04:18:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombre=prompt("indique su nombre")
+let edad=parseInt(prompt("indique la edad"));
+
+let edad2 = 18
+if (edad>edad2){
+  console.log("usted es mayor de edad")
+}
+else{
+  console.log("usted es menor de edad")
+}
